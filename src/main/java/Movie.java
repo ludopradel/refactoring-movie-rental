d8b8e2b8a065c9d9643db756a12a659986de5d42
ludopradel <ludovic.pradel@gmail.com>
2017-02-19 21:24:20 +0100

    moving getCharge and frequent renter points to Movie class

--- src/main/java/Movie.java
+++ src/main/java/Movie.java
@@ -22,4 +22,30 @@ public class Movie {
     public String getTitle() {
         return _title;
     }
+
+    public double getCharge(int daysRented) {
+        double result = 0;
+        switch (_priceCode) {
+            case Movie.REGULAR :
+                result += 2;
+                if (daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
+                break;
+        }
+        return result;
+    }
+
+    public int getFrequentRenterPoints(int daysRented) {
+        if ((_priceCode == Movie.NEW_RELEASE) && daysRented > 1)
+            return 2;
+        return 1;
+    }
 }
--- src/main/java/Rental.java
+++ src/main/java/Rental.java
@@ -12,28 +12,10 @@ public class Rental {
     }
 
     public double getCharge() {
-        double result = 0;
-        switch (_movie.getPriceCode()) {
-            case Movie.REGULAR :
-                result += 2;
-                if (_daysRented > 2)
-                    result += (_daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += _daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (_daysRented > 3)
-                    result += (_daysRented - 3) * 1.5;
-                break;
-        }
-        return result;
+        return _movie.getCharge(_daysRented);
     }
 
     public int getFrequentRenterPoints() {
-        if ((_movie.getPriceCode() == Movie.NEW_RELEASE) && _daysRented > 1)
-            return 2;
-        return 1;
+        return _movie.getFrequentRenterPoints(_daysRented);
     }
 }
